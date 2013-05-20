7d203946341be164af1c77335aa1ac8ac913e007
John <none@example.com>
2013-05-20 19:56:43 +0700

    Add hi consonants, add const class mark

--- list.js
+++ list.js
@@ -12,32 +12,44 @@ numbers = [
 ]
 
 lowConsonants1 = [
-	{ english: "n", thai: "น", pronunciation: "no" },
-	{ english: "m", thai: "ม", pronunciation: "mo" },
-	{ english: "ng", thai: "ง", pronunciation: "ngo" },
-	{ english: "r", thai: "ร", pronunciation: "ro" },
-	{ english: "l", thai: "ล", pronunciation: "lo" },
-	{ english: "y", thai: "ย", pronunciation: "yo" },
-	{ english: "w", thai: "ว", pronunciation: "wo" }
+	{ english: "n", thai: "น", pronunciation: "no (low)" },
+	{ english: "m", thai: "ม", pronunciation: "mo (low)" },
+	{ english: "ng", thai: "ง", pronunciation: "ngo (low)" },
+	{ english: "r", thai: "ร", pronunciation: "ro (low)" },
+	{ english: "l", thai: "ล", pronunciation: "lo (low)" },
+	{ english: "y", thai: "ย", pronunciation: "yo (low)" },
+	{ english: "w", thai: "ว", pronunciation: "wo (low)" }
 ]
 
 lowConsonants2 = [
-	{ english: "k", thai: "ค", pronunciation: "ko" },
-	{ english: "ch", thai: "ช", pronunciation: "cho" },
-	{ english: "s", thai: "ซ", pronunciation: "so" },
-	{ english: "t", thai: "ท", pronunciation: "to" },
-	{ english: "p", thai: "พ", pronunciation: "po" },
-	{ english: "f", thai: "ฟ", pronunciation: "fo" }
+	{ english: "k", thai: "ค", pronunciation: "ko (low)" },
+	{ english: "ch", thai: "ช", pronunciation: "cho (low)" },
+	{ english: "s", thai: "ซ", pronunciation: "so (low)" },
+	{ english: "t", thai: "ท", pronunciation: "to (low)" },
+	{ english: "p", thai: "พ", pronunciation: "po (low)" },
+	{ english: "f", thai: "ฟ", pronunciation: "fo (low)" }
 ]
 
 midConsonants = [
-	{ english: "g", thai: "ก", pronunciation: "go" },
-	{ english: "j", thai: "จ", pronunciation: "jo" },
-	{ english: "d", thai: "ด", pronunciation: "do" },
-	{ english: "dt", thai: "ต", pronunciation: "dto" },
-	{ english: "b", thai: "บ", pronunciation: "bo" },
-	{ english: "bp", thai: "ป", pronunciation: "bpo" },
-	{ english: "o", thai: "อ", pronunciation: "oh" }
+	{ english: "g", thai: "ก", pronunciation: "go (mid)" },
+	{ english: "j", thai: "จ", pronunciation: "jo (mid)" },
+	{ english: "d", thai: "ด", pronunciation: "do (mid)" },
+	{ english: "dt", thai: "ต", pronunciation: "dto (mid)" },
+	{ english: "b", thai: "บ", pronunciation: "bo (mid)" },
+	{ english: "bp", thai: "ป", pronunciation: "bpo (mid)" },
+	{ english: "oh", thai: "อ", pronunciation: "oh (mid)" }
+]
+ 
+hiConsonants = [
+	{ english: "k", thai: "ข", pronunciation: "ko (hi)" },
+	{ english: "ch", thai: "ฉ", pronunciation: "cho (hi)" },
+	{ english: "t", thai: "ถ", pronunciation: "to (hi)" },
+	{ english: "p", thai: "ผ", pronunciation: "po (hi)" },
+	{ english: "f", thai: "ฝ", pronunciation: "fo (hi)" },
+	{ english: "s", thai: "ศ", pronunciation: "so (hi)" },
+	{ english: "s", thai: "ษ", pronunciation: "so (hi)" },
+	{ english: "s", thai: "ส", pronunciation: "so (hi)" },
+	{ english: "h", thai: "ห", pronunciation: "ho (hi)" }
 ]
  
 longVowels = [
@@ -54,7 +66,8 @@ list = [
 	lowConsonants1,
 	lowConsonants2,
 	midConsonants,
-	longVowels
+	longVowels,
+	hiConsonants
 ]
  
  
